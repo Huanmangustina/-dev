9e415d8fb50dbedce83100ff7f681cb216bdf1b4
Huanmangustina <b75@list.ru>
2021-03-27 23:27:19 +0300

    comments

--- 60 flow 09032021/Unit_0/Final_task/ZeroUnu.py
+++ 60 flow 09032021/Unit_0/Final_task/ZeroUnu.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+# зададим поле.
+# Поле у нас будет одномерным списком (list) с числами от 1 до 9.
+# Для создания воспользуемся функцией range()
 
 board = list(range(1, 10))
 
 
-def draw_board(board):
+# напишем функцию, которая будет рисовать игровое поле.
+def draw_board (board):
     print("-" * 13)
     for i in range(3):
         print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
         print("-" * 13)
 
 
+# Формируем возможность ввода пользователем  данных в игру.
 def take_input(player_token):
     valid = False
     while not valid:
@@ -32,6 +32,10 @@ def take_input(player_token):
         else:
             print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
 
+# задаем кортедж выйгрышных значений и цикл проверки текущих значений
+# Если символы во всех трех заданных клетках равны - возвращаем выигрышный символ,
+# иначе - возвращаем значение False.
+# непустая строка (наш выигрышный символ) при приведении ее к логическому типу вернет True
 
 def check_win(board):
     win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
@@ -40,7 +44,9 @@ def check_win(board):
             return board[each[0]]
     return False
 
-
+# Главная функция, в которой происходит игра
+# счетчик чередует возможность ввода Х и О
+# всего не более 9 ходов
 def main(board):
     counter = 0
     win = False
@@ -51,8 +57,8 @@ def main(board):
         else:
             takeInput("O")
         counter += 1
-        if counter > 4:
-            tmp = check_win(board)
+        if counter > 4: # выигрыш не возможен до 5 хода
+            tmp = check_win(board) #  чтобы не вызывать функцию check_win
             if tmp:
                 print(tmp, "выиграл!")
                 win = True
@@ -65,61 +71,4 @@ def main(board):
 
 main(board)
 
-# In[ ]:
 
-
-board = list(range(1, 10))
-
-
-def takeInput(player_token):
-    valid = False
-    while not valid:
-        player_answer = input("Куда поставим " + player_token + "? ")
-        try:
-            player_answer = int(player_answer)
-        except:
-            print("Некорректный ввод. Вы уверены, что ввели число?")
-            continue
-        if player_answer >= 1 and player_answer <= 9:
-            if (str(board[player_answer - 1]) not in "XO"):
-                board[player_answer - 1] = player_token
-                valid = True
-            else:
-                print("Эта клеточка уже занята")
-        else:
-            print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
-
-
-def check_win(board):
-    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    for each in win_coord:
-        if board[each[0]] == board[each[1]] == board[each[2]]:
-            return board[each[0]]
-    return False
-
-
-def main(board):
-    counter = 0
-    win = False
-    while not win:
-        draw_board(board)
-        if counter % 2 == 0:
-            takeInput("X")
-        else:
-            takeInput("O")
-        counter += 1
-        if counter > 4:
-            tmp = check_win(board)
-            if tmp:
-                print(tmp, "выиграл!")
-                win = True
-                break
-        if counter == 9:
-            print("Ничья!")
-            break
-    draw_board(board)
-
-
-main(board)
-
-# In[ ]:
