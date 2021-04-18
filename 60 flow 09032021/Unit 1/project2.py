c6e7fd06036f86d7b449b619bd317b9922a5a046
Huanmangustina <b75@list.ru>
2021-04-18 22:57:17 +0300

    football

--- 60 flow 09032021/Unit 1/project2.py
+++ 60 flow 09032021/Unit 1/project2.py
@@ -1,5 +1,9 @@
+from typing import Any, Union
+
 import pandas as pd
 from IPython.core.display import display
+from pandas import Series, DataFrame
+from pandas.io.parsers import TextFileReader
 
-football = pd.read_csv('data_sf.csv')
-display(football.info())
+football: Union[Union[TextFileReader, Series, DataFrame, None], Any] = pd.read_csv("data_sf.csv")
+football[football.Age.max()].Reactions.mean()
